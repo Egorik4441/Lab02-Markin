85e6f1c83bb231b4e305efdbbff2cbe8a34e3938
karasidnez <karasdnez@gmail.com>
2026-09-11 13:07:16 +0300

    Lab02:char and decimal precision

--- obj/Debug/net10.0/Lab02-Markin.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Markin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Markin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0cdbd38ff28782f77fdf78943cb29e4e86c271f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Markin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Markin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
